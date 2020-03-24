aa5591adbc78831c821f88e5ab35954e90745d0f
SaulXibille <49504706+SaulXibille@users.noreply.github.com>
2020-03-23 18:11:15 -0700

    asdsadasd

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+// import { HttpModule } from '@angular/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Http } from '@angular/http';
+// import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  logindata:any = {};
+
+  constructor(public http: HttpClient) { 
+    this.logindata.correo = "";
+    this.logindata.contrasena = "";
+  }
 
   ngOnInit() {
+  }
+
+  Login() {
+    if(this.logindata.correo != "" && this.logindata.contrasena != ""){
+      console.log("user: ", this.logindata.correo);
+      console.log("password: ", this.logindata.contrasena);
+
+      let url:string = "https://fixing.ejemplo.com/login.php";
+      let dataPost = {
+          correo : this.logindata.correo,
+          contrasena : this.logindata.contrasena
+      };
+
+      // this.http.post(url,dataPost)
+      // .map(res => res.json())
+      // .subscribe(data => {
+      //   console.log(data);
+      // });
+      console.log(dataPost);
+
+      this.http.post("http://fixing.ejemplo.com/login.php", JSON.stringify(dataPost)).toPromise().then((data:any) => {
+        console.log(data);
+      });
+
+    }else {
+      console.log("Ingrese una contraseña");
+    }
   }
 
 }
