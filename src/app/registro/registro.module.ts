886f7a5c87e5843ae55aca59a1f6af39ee709f0c
Frida <fridamzt26@gmail.com>
2020-04-09 00:35:20 -0600

    jeje

--- src/app/registro/registro.module.ts
+++ src/app/registro/registro.module.ts
@@ -2,17 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
+
 import { IonicModule } from '@ionic/angular';
+
 
 import { RegistroPageRoutingModule } from './registro-routing.module';
 
 import { RegistroPage } from './registro.page';
+import { HttpModule } from '@angular/http';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
+    HttpModule,
     RegistroPageRoutingModule
   ],
   declarations: [RegistroPage]
--- src/app/registro/registro.page.ts
+++ src/app/registro/registro.page.ts
@@ -1,15 +1,79 @@
 import { Component, OnInit } from '@angular/core';
+import { Http,Response } from '@angular/http';
+//import { HttpClient } from '@angular/common/http';
+import { PostService } from '../Servicios/post.service';
+import 'rxjs/Rx';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.page.html',
   styleUrls: ['./registro.page.scss'],
 })
-export class RegistroPage implements OnInit {
+export class RegistroPage {
+  correo:String ="";
+  contrasenia:String ="";
+  confirmarContrasenia:String ="";
 
-  constructor() { }
+  constructor( private http:Http, private postSer:PostService, public alertController: AlertController) { 
+    
+  }
 
-  ngOnInit() {
+  ionViewDidLoad() {
+    
+  }
+
+  async registro(){
+    if(this.contrasenia == this.confirmarContrasenia){
+        let body = {
+          correo: this.correo,
+          opc: 'usuario',
+        };
+        this.postSer.postData(body, 'registro.php').subscribe(async data =>{
+          if(data.success){
+            let mensaje="El correo ya se encuentra registrado.";
+            this.alerta(mensaje);
+          }     
+          else{
+              let user = {
+              correo: this.correo,
+              contrasenia: this.contrasenia,
+              opc: 'insertar'
+              };
+
+              this.postSer.postData(user, 'registro.php').subscribe(async response =>{
+                console.log(response);
+              });
+          }
+        });
+    }
+    else{
+      let mensaje = "La contraseña no coincide.";
+      this.alerta(mensaje);
+    }
+    // let body = {
+    //   correo: this.correo,
+    //   contrasenia: this.contrasenia,
+    // };
+    // const{correo,contrasenia,confirmarContrasenia}=this;
+    // this.http.post("http://localhost/appdata/registro.php", JSON.stringify({correo,contrasenia,confirmarContrasenia}))
+    //         .subscribe((response) => {
+    //             console.log(response);
+    //             return response.json();
+    //         })
+    // this.postSer.postData(body, 'registro.php').subscribe(async response =>{
+    //   console.log(response);
+    // });
+  }
+
+  async alerta(mensaje) {
+    const alert = await this.alertController.create({
+      header: 'Error',
+      message: mensaje,
+      buttons: ['Aceptar']
+    });
+
+    await alert.present();
   }
 
 }
