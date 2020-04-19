2156639d02f7df271eb766a894c4e3483fc2178d
Frida <fridamzt26@gmail.com>
2020-04-19 14:53:40 -0600

    ya funciona creo im sorry

--- src/app/perfil/perfil.page.ts
+++ src/app/perfil/perfil.page.ts
@@ -40,7 +40,7 @@ export class PerfilPage implements OnInit {
     this.storage.get('idUsuario').then((val)=>{
       this.idUsuario = val;
       console.log(val);
-      // this.validar();
+      this.validar();
     });
   }
 
@@ -52,15 +52,15 @@ export class PerfilPage implements OnInit {
     this.postSer.postData(body, 'datosusuario.php').subscribe(async data =>{
        console.log(data);
        if(data.success){
-        // this.nombre = data.result['nombre'];
-        // this.telefono = data.result['telefono'];
-        // this.numExterior= data.result['num_ext'];
-        // this.numInterior = data.result['num_int'];
-        // this.referencias = data.result['referencias'];
-        // this.codigoPostal = data.result['codigo_postal'];
-        // this.colonia = data.result['colonia'];
-        // this.calle = data.result['calle'];
-        // this.correo = data.result['correo'];
+        this.nombre = data.result['nombre'];
+        this.telefono = data.result['telefono'];
+        this.numExterior= data.result['num_ext'];
+        this.numInterior = data.result['num_int'];
+        this.referencias = data.result['referencias'];
+        this.codigoPostal = data.result['codigo_postal'];
+        this.colonia = data.result['colonia'];
+        this.calle = data.result['calle'];
+        this.correo = data.result['correo'];
        }
     });
   }
@@ -68,20 +68,41 @@ export class PerfilPage implements OnInit {
 
 
   async insertarDatos(){
-    let body = {
-      nombre: this.nombre,
-      telefono: this.telefono,
-      idUsuario: this.idUsuario,
-      opc: 'insertar',
-    };
-    console.log(body['telefono']);
-    this.postSer.postData(body, 'datosusuario.php').subscribe(async data =>{
-      console.log(data);
-    });
+    if(this.nombre != "" && this.nombre != null && this.telefono != "" && this.numInterior != "" && this.numExterior != "" 
+    && this.referencias != "" && this.codigoPostal != "" && this.colonia != "" && this.calle != "" && this.correo != ""){
+      let body = {
+        nombre: this.nombre,
+        telefono: this.telefono,
+        num_int:this.numInterior,
+        num_ext:this.numExterior,
+        referencias: this.referencias,
+        codigo_postal: this.codigoPostal,
+        colonia: this.colonia,
+        calle: this.calle,
+        idUsuario: this.idUsuario,
+        opc: 'insertar',
+      };
+      console.log(body['telefono']);
+      this.postSer.postData(body, 'datosusuario.php').subscribe(async data =>{
+        console.log(data);
+      });
+    }
+    else{
+        this.presentToast("Todos los campos son obligatorios.");
+    }
+    
   }
 
   toggleMenu(){
     this.menuCtrl.toggle();
+  }
+
+  async presentToast(mensaje) {
+    const toast = await this.toastController.create({
+      message: mensaje,
+      duration: 2000
+    });
+    toast.present();
   }
 
 }
